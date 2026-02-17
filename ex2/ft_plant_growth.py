3c1e4d55dfa0571c128ec5f471d46de9548aae64
Adriana Neves <158789061+adriananevex@users.noreply.github.com>
2026-02-17 18:53:42 +0000

    Implement Plant class with growth and aging methods

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -1,1 +1,30 @@
 class Plant:
+  def __init__(self, name: str, height: int, age: int) -> None:
+    self.name: str = name
+    self.height: int = height
+    self.age: int = age
+
+def grow(self) -> None:
+  self.height += 1
+
+def age_one_day(self) -> None:
+  self.age += 1
+
+def get_info(self) -> str:
+  return f"{self.name}: {self.height}cm, {self.age} days old"
+
+def main() -> None:
+  rose = Plant("Rose", 25, 30)
+
+  print("=== Day 1 ===")
+  print(rose.get_info())
+
+  for i in range(6):
+    rose.grow()
+    rose.age_one_day()
+
+  print("=== Day 7 ===")
+  print(rose.get_info())
+
+if __name__ == "__main__":
+  main()
